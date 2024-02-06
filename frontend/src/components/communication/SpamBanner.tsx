67cd5980f8afb50b1ae9b8f43596d53c9239e8da
cirko0 <cirko007ivan@gmail.com>
2024-02-06 21:19:24 +0100

    added SpamBanner

---
+++ frontend/src/components/communication/SpamBanner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SpamBanner: React.FC = () => {
+  return (
+    <div className="bg-yellow-100 font-medium text-yellow-900 py-3 px-4 rounded-xl flex flex-col md:flex-row md:items-center gap-2 md:w-[95%]">
+      <p className="font-semibold shrink-0 md:mr-1">
+        <i className="fa-solid fa-triangle-exclamation" />
+        &thinsp; Spam Policy
+      </p>
+      <p className="text-sm md:mt-1">
+        Please submit valid food recipes only - this helps maintain a pleasant
+        experience for everyone and ensure that things work as intended.
+        Expletives are strictly prohibited.
+      </p>
+    </div>
+  );
+};
+
+export default SpamBanner;
